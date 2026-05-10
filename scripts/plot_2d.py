ae30038fd8c092982ece118f7b4d8068b4b23701
admin <nnamdichibuzor777@gmail.com>
2026-05-09 20:55:14 -0700

    add 3d plots

---
+++ scripts/plot_2d.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from plot_util import init_plot
+
+def main():
+
+    args, data, ref_data, t, nplots, labels, save_fpath = init_plot(spatial=False)
+
+    plt.figure(figsize=(10, 3 * nplots))
+
+    for i in range(nplots):
+        plt.subplot(nplots, 1, i + 1)
+        plt.plot(t, data[:, i + 1], linewidth=2, label="data")
+        plt.xlabel("t [s]")
+        plt.ylabel(labels[i] if i < len(labels) else f"data {i}")
+        plt.grid()
+        
+        if ref_data is not None:
+            plt.plot(t, ref_data[:, i + 1], linewidth=2, label="reference", linestyle="--")
+            plt.legend()
+
+    plt.suptitle(args.title)
+    plt.tight_layout()
+    plt.savefig(save_fpath)
+    plt.close()
+
+    print(f"File saved successfully to {save_fpath}")
+
+if __name__ == "__main__":
+    main()
--- scripts/plot_util.py
+++ scripts/plot_util.py
@@ -1,10 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import argparse
 from datetime import datetime
 
-def main():
+def init_plot(spatial=False):
     parser = argparse.ArgumentParser(description="Plotting CLI arguments")
     parser.add_argument("--data", "-d", type=str, help="Data file path")
     parser.add_argument("--ref", "-r", type=str, help="Reference data file path")
@@ -13,7 +12,6 @@ def main():
     parser.add_argument("--title", "-t", type=str, help="Title")
     args = parser.parse_args() 
 
-
     data_fpath = f"data/{args.data}"
     ref_data_fpath = f"data/{args.ref}" if args.ref is not None else None
     save_dir = f"figures/{os.path.basename(os.path.dirname(data_fpath))}"
@@ -21,6 +19,8 @@ def main():
     dtn = datetime.now().strftime("%Y%b%d_%H-%M-%S")
 
     save_fname = os.path.splitext(os.path.basename(data_fpath))[0]
+    if spatial: 
+        save_fname += "_3d" 
     save_fpath = os.path.join(save_dir, f"{save_fname}.{args.ext}")
 
     data = np.loadtxt(data_fpath, delimiter=",")
@@ -30,25 +30,4 @@ def main():
     nplots = data.shape[1] - 1
     labels = args.labels[:nplots]
 
-    plt.figure(figsize=(10, 3 * nplots))
-
-    for i in range(nplots):
-        plt.subplot(nplots, 1, i + 1)
-        plt.plot(t, data[:, i + 1], linewidth=2, label="data")
-        plt.xlabel("t [s]")
-        plt.ylabel(labels[i] if i < len(labels) else f"data {i}")
-        plt.grid()
-        
-        if ref_data is not None:
-            plt.plot(t, ref_data[:, i + 1], linewidth=2, label="reference", linestyle="--")
-            plt.legend()
-
-    plt.suptitle(args.title)
-    plt.tight_layout()
-    plt.savefig(save_fpath)
-    plt.close()
-
-    print(f"File saved successfully to {save_fpath}")
-
-if __name__ == "__main__":
-    main()
+    return args, data, ref_data, t, nplots, labels, save_fpath
