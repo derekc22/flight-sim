ad0e04a68456e5b09f917d3264eb211f20df3c98
admin <nnamdichibuzor777@gmail.com>
2026-09-20 16:24:50 -0700

    seed launch script with lat/lon from initialization config

--- scripts/lib/launch.sh
+++ scripts/lib/launch.sh
@@ -5,6 +5,12 @@ source .env
 
 AIRCRAFT="${1:?usage: $0 <AIRCRAFT>}"
 
+RUN_JSON="config/run.json"
+INITIALIZATION_JSON="config/$(jq -r '.initialization_config' "$RUN_JSON")"
+
+LAT=$(jq -r '.NEDFrameECEF.lat' "$INITIALIZATION_JSON")
+LON=$(jq -r '.NEDFrameECEF.lon' "$INITIALIZATION_JSON")
+
 exec "$FG_PATH" \
 --ignore-autosave \
 --generic=socket,out,100,127.0.0.1,5510,udp,udp-out \
@@ -18,4 +24,7 @@ exec "$FG_PATH" \
 --geometry=800x700 \
 --prop:/engines/engine[0]/rpm=2300 \
 --httpd=8080 \
---data="$PROJ_PATH"/schema
+--data="$PROJ_PATH"/schema \
+--disable-sound \
+--lat="$LAT" \
+--lon="$LON" \
